a8e04ec1d0142e05337b2f9cc86ad99226b16717
lighttttt <yxu70832@usc.edu>
2022-09-10 21:25:15 -0400

    Initial Commit

---
+++ Gather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Gather
+//
+//  Created by Yi Xu on 9/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Gather/GatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  GatherApp.swift
+//  Gather
+//
+//  Created by Yi Xu on 9/10/22.
+//
+
+import SwiftUI
+
+@main
+struct GatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
