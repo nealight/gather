82f59d463bd10e5ad38cd07a4593c31d857eb15b
lighttttt <yxu70832@usc.edu>
2022-11-06 22:04:56 -0500

    Fixed minor bug

--- Gather/Services/NetworkResponses/SigninNetworkResponseModel.swift
+++ Gather/Services/NetworkResponses/SigninNetworkResponseModel.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct SigninNetworkResponseModel: Codable {
     var message: String
-    var user_name: String
+    var user_name: String?
     var uploadImageURL: String?
     var downloadImageURL: String?
     var token: String?
